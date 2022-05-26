5abe8bb122b1d83e82b1dab0f6be464c3166c37a
Austin <austin.gilmore@codingscape.com>
2022-05-26 15:21:21 -0700

    simplified day 7

--- day_7/solution.py
+++ day_7/solution.py
@@ -6,74 +6,24 @@ def parse_text():
         return list(map(lambda x: int(x), f.read().split(',')))
 
 
-def populate_init_distance(crabs):
-    distances = [0] * (max(crabs) + 1)
-    for crab in crabs:
-        distances[crab] += 1
-    return distances
+def part_1(crabs):
+    sums = []
+    for i in range(max(crabs)):
+        sums.append(sum([abs(i - c) for c in crabs]))
+    print(min(sums))
 
 
-def rotate_distance(distances, center):
-    add = distances[0:center]
-    for i in range(0, center):
-        distances = distances[1:len(distances)] + [0]
-
-    for i in range(0, len(add)):
-        distances[center] += add[i]
-        center -= 1
-
-    return distances
-
-
-def populate_all_distances(init_distance):
-    distances = []
-    distances.append(init_distance)
-    for i in range(1, len(init_distance)):
-        distances.append(rotate_distance(init_distance, i))
-    return distances
-
-
-# part 1
-def calculate_fuel_cost(distance_list):
-    sum = 0
-    for i in range(0, len(distance_list)):
-        sum += (i * distance_list[i])
-    return sum
-
-
-# part 1
-def get_min_fuel_cost(distances):
-    fuel_costs = list(map(calculate_fuel_cost, distances))
-    return min(fuel_costs)
-
-
-# part 2
-def calculate_fuel_cost_2(distance_list):
-    sum = 0
-    for i in range(0, len(distance_list)):
-        cost = (i * (i + 1))/2
-        sum += (cost * distance_list[i])
-    return sum
-
-
-# part 2
-def get_min_fuel_cost_2(distances):
-    fuel_costs = list(map(calculate_fuel_cost_2, distances))
-    return min(fuel_costs)
+def part_2(crabs):
+    sums = []
+    for i in range(max(crabs)):
+        sums.append(sum([(abs(i - c) * (abs(i - c) + 1))/2 for c in crabs]))
+    print(min(sums))
 
 
 def main():
     crabs = parse_text()
-    init_distance = populate_init_distance(crabs)
-    distances = populate_all_distances(init_distance)
-
-    #part 1
-    min = get_min_fuel_cost(distances)
-    print(min)
-
-    #part 2
-    min2 = get_min_fuel_cost_2(distances)
-    print(min2)
+    part_1(crabs)
+    part_2(crabs)
 
 
 if __name__ == "__main__":
