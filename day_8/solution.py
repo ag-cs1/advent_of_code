d2cd9afb872ed1f26c811030a004fc4674988c66
Austin <austin.gilmore@codingscape.com>
2022-05-27 14:53:53 -0700

    removed unused dicts

--- day_8/solution.py
+++ day_8/solution.py
@@ -1,28 +1,3 @@
-# key = num of segments
-# values = numbers with num of segments
-segment_dict = {
-    2: [1],
-    3: [7],
-    4: [4],
-    5: [2, 3, 5],
-    6: [0, 6, 9],
-    7: [8],
-}
-
-number_dict = {
-    0: ['a', 'b', 'c', 'e', 'f', 'g'],
-    1: ['c', 'f'],
-    2: ['a', 'c', 'd', 'e', 'g'],
-    3: ['a', 'c', 'd', 'f', 'g'],
-    4: ['b', 'c', 'd', 'f'],
-    5: ['a', 'b', 'd', 'f', 'g'],
-    6: ['a', 'b', 'd', 'e', 'f', 'g'],
-    7: ['a', 'c', 'f'],
-    8: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
-    9: ['a', 'b', 'c', 'd', 'f', 'g']
-}
-
-
 def parse_text():
     with open("input.txt") as f:
         output_lists = [line.split(" | ")[1].split(" ") for line in f.read().split("\n")]
